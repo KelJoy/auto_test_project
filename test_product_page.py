7440b497aa006cf3de710c880235eecb47379b7e
KelJoy <vadim_kravchenjo2000@mail.ru>
2022-11-01 14:23:04 +0300

    add tests for product and main pages

--- test_product_page.py
+++ test_product_page.py
@@ -1,6 +1,7 @@
 from .pages.product_page import ProductPage
 import pytest
 from .pages.login_page import LoginPage
+from .pages.basket_page import BasketPage
 import time
 '''
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
@@ -22,7 +23,7 @@ def test_guest_can_go_to_product(browser, link):
 '''
 
 
-
+'''
 @pytest.mark.xfail
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
 	link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
@@ -62,3 +63,12 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
 	page.go_to_login_page()
 	login_page = LoginPage(browser, browser.current_url)
 	login_page.should_be_login_page()
+'''
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+	link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+	page = ProductPage(browser, link)
+	page.open()
+	page.go_to_basket()
+	basket_page=BasketPage(browser, browser.current_url)
+	basket_page.basket_should_be_empty()
+	basket_page.should_be_message_about_empty_basket()
