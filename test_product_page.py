029539f852dc1ca05d6075aacd5e1421c5acc3a1
KelJoy <vadim_kravchenjo2000@mail.ru>
2022-11-01 17:09:30 +0300

    final version

--- test_product_page.py
+++ test_product_page.py
@@ -35,7 +35,7 @@ class TestUserAddToBasketFromProductPage():
 		page.check_product_name()
 		page.check_product_price()
 
-
+@pytest.mark.need_review
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
 								  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
 								  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
@@ -46,12 +46,13 @@ class TestUserAddToBasketFromProductPage():
 								  pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
 								  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
 								  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
-@pytest.mark.need_review
-def test_guest_can_go_to_product(browser, link):
-    page = ProductPage(browser, link)
-    page.open()
-    page.add_product_to_basket()
-    page.solve_quiz_and_get_code()
+def test_guest_can_add_product_to_basket(browser, link):
+	page = ProductPage(browser, link)
+	page.open()
+	page.add_product_to_basket()
+	page.solve_quiz_and_get_code()
+	page.check_product_name()
+	page.check_product_price()
 
 
 
