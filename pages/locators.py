f177620a294b302b58555af89743931f2bfe7a4a
KelJoy <vadim_kravchenjo2000@mail.ru>
2022-11-01 15:18:28 +0300

    add fixtures and create some classes of tests

--- pages/locators.py
+++ pages/locators.py
@@ -7,9 +7,6 @@ class BasePageLocators():
     USER_ICON = (By.CSS_SELECTOR, ".icon-user")
     BASKET_LINK = (By.CSS_SELECTOR, ".btn-group a.btn.btn-default")
 
-class MainPageLocators():
-    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
-
 
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
--- test_product_page.py
+++ test_product_page.py
@@ -3,6 +3,38 @@ import pytest
 from .pages.login_page import LoginPage
 from .pages.basket_page import BasketPage
 import time
+
+class TestUserAddToBasketFromProductPage():
+	@pytest.fixture(scope="function", autouse=True)
+	def setup(self, browser):
+		#
+		link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+		login_page=LoginPage(browser, link)
+		login_page.open()
+		login_page.go_to_login_page()
+		email = str(time.time()) + "@fakemail.org"
+		password = "128mb256kb512gb"
+		login_page.register_new_user(email, password)
+		login_page.should_be_authorized_user()
+	
+	def test_user_cant_see_success_message(self, browser):
+		#
+		link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+		page = ProductPage(browser, link)
+		page.open()
+		page.should_not_be_success_message()
+
+	
+	def test_user_can_add_product_to_basket(self, browser):
+		#
+		link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+		page = ProductPage(browser, link)
+		page.open()
+		page.add_product_to_basket()
+		page.solve_quiz_and_get_code()
+		page.check_product_name()
+		page.check_product_price()
+
 '''
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
 								  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
@@ -34,6 +66,7 @@ def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
 	page.should_not_be_success_message()
 
 def test_guest_cant_see_success_message(browser):
+	print("les4.3.6-2")
 	link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
 	page = ProductPage(browser, link)
 	page.open()
@@ -63,7 +96,7 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
 	page.go_to_login_page()
 	login_page = LoginPage(browser, browser.current_url)
 	login_page.should_be_login_page()
-'''
+
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
 	link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
 	page = ProductPage(browser, link)
@@ -72,3 +105,4 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
 	basket_page=BasketPage(browser, browser.current_url)
 	basket_page.basket_should_be_empty()
 	basket_page.should_be_message_about_empty_basket()
+'''
